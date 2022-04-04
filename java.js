036bb75c13e7c328868ae757ba1f0e4c49ce509f
Joey Lundquist <joeylundquist91@gmail.com>
2022-04-04 11:10:33 -0500

    finished practice

---
+++ java.js
@@ -0,0 +1,16 @@
+console.log('Hello Page!!');
+
+//declare and initialize a variable
+let testVar = 9;
+
+console.log(testVar)
+
+console.log('the value of testVar is', testVar)
+
+testVar = 54675489;
+
+console.log('the value of testVar is', testVar)
+
+testVar = 'some text instead!!'
+
+console.log(testVar);
